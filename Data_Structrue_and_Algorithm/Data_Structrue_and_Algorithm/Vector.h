52ea82884f1a9f6edd12688408bda0a80d726115
杨汇琛 <32983694+Huic-Yang@users.noreply.github.com>
2017-11-09 15:43:15 +0800

    inplement version 1 without debug and test

--- Data_Structrue_and_Algorithm/Data_Structrue_and_Algorithm/Vector.h
+++ Data_Structrue_and_Algorithm/Data_Structrue_and_Algorithm/Vector.h
@@ -30,8 +30,18 @@ class Vector {
   Vector<T> & operator=(const Vector<T> &V);
   T remove(Rank r);
   int remove(Rank lo, Rank hi);
+  Rank insert(Rank r, const T &e);
+  Rank insert(const T &e) { return insert(_size, e); }
+  void sort();
+  void unsort(Rank lo, Rank hi);
+  int deduplicate();
+  int uniquify();
+  void traverse(void (*)(T&));
+  template<typename VST> void traverse(VST&);
  protected:
   void copy_from(const T *A, Rank lo, Rank hi);
+  void expand();
+  void shrink();
 };
 
 template<typename T>
@@ -76,7 +86,7 @@ int Vector::disordered() const {
 
 template<typename T>
 Rank Vector::find(const T &e, Rank lo, Rank hi) const {
-  for (Rank i = lo; i < hi; ++i) 
+  for (Rank i = lo; i < hi; ++i)
     if (_elem[i] == e)
       return i;
   return -1;
@@ -103,17 +113,48 @@ template<typename T>
 int Vector::remove(Rank lo, Rank hi) {
   for (Rank p = lo + 1, q = hi; q < _size; ++p, ++q)
     _elem[p] = _elem[q];
+  shrink();
   return hi - lo;
 }
 
-//
+template<typename T>
+Rank Vector::insert(Rank r, const T &e) {
+  if (_size == _capacity) expand();
+  for (Rank p = _size; r < p; --p)
+    _elem[p] = _elem[p - 1];
+  _elem[r] = e;
+  return r + 1;
+}
 
+// protected methods
 template<typename T>
 void Vector::copy_from(const T *A, Rank lo, Rank hi) {
   if (_elem) delete[] _elem;
   _elem = new T[_capacity = (hi - lo) << 1];
-  for (_size = 0; _size < hi - lo; ++_size) 
+  for (_size = 0; _size < hi - lo; ++_size)
     _elem[_size] = T[hi + _size];
+}
+
+template<typename T>
+void Vector::expand() {
+  if (_size < _capacity) return;
+  T *old_elem = _elem;
+  if (_capacity < kDEFAULT_CAPACITY) _capacity = kDEFAULT_CAPACITY;
+  _elem = new T[_capacity <<= 1];
+  for (int sz = 0; sz < _size; ++sz)
+    _elem[sz] = old_elem[sz];
+  delete[] old_elem; old_elem = NULL;
+}
+
+template<typename T>
+void Vector::shrink() {
+  if (_capacity <= kDEFAULT_CAPACITY << 1 || _capacity < _size << 2)
+    return;
+  T *old_elem = _elem;
+  _elem = new T[_capacity >>= 1];
+  for (Rank i = 0; i < _size; ++i)
+    _elem[i] = old_elem[i];
+  delete[] old_elem; old_elem = NULL;
 }
 
 #endif // VECTOR_H_
